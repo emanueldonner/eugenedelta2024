656c6234719e85a9a8e48e9c273628bea193b9a0
emanueldonner <emanuel.donner@wh-d.at>
2024-05-26 15:48:41 +0200

    Added things

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ function togglePlay() {
 	const playButton = document.querySelector(".play-button")
 
 	if (audio.paused) {
+		stopIframePlayback() // Stop video playback when audio is played
 		audio.play()
 		playButton.innerHTML =
 			'<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M14 19V5h4v14zm-8 0V5h4v14z"/></svg>' // Pause icon
@@ -30,11 +31,35 @@ function setProgress(event) {
 	audio.currentTime = (clickX / width) * duration
 }
 
+function stopIframePlayback() {
+	const iframes = document.querySelectorAll(".carousel-item iframe")
+	iframes.forEach((iframe) => {
+		iframe.src = ""
+		iframe.style.display = "none"
+		const thumbnail = iframe.previousElementSibling.previousElementSibling
+		const playButton = iframe.previousElementSibling
+		thumbnail.style.display = "block"
+		playButton.style.display = "block"
+	})
+}
+
+function stopAudioPlayback() {
+	const audio = document.getElementById("audio")
+	const playButton = document.querySelector(".play-button")
+
+	if (!audio.paused) {
+		audio.pause()
+		playButton.innerHTML =
+			'<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M8 5.14v14l11-7z" /></svg>' // Play icon
+	}
+}
+
 function loadSong(src, title, element) {
 	const audio = document.getElementById("audio")
 	const playButton = document.querySelector(".play-button")
 	const playlistItems = document.querySelectorAll(".playlist li")
 
+	stopIframePlayback() // Stop video playback when a song starts
 	audio.src = src
 	audio.play()
 	playButton.innerHTML =
@@ -57,12 +82,14 @@ function closeModal() {
 }
 
 // Carousel functionality
+
 function showIframe(wrapper) {
 	const iframe = wrapper.querySelector("iframe")
 	const thumbnail = wrapper.querySelector("img")
 	const playButton = wrapper.querySelector(".play-button-overlay")
 	const videoSrc = iframe.getAttribute("data-src")
 
+	stopAudioPlayback() // Stop audio playback when a video starts
 	thumbnail.style.display = "none"
 	playButton.style.display = "none"
 	iframe.style.display = "block"
@@ -73,6 +100,7 @@ let currentSlide = 0
 const slides = document.querySelectorAll(".carousel-item")
 
 function showSlide(index) {
+	stopIframePlayback()
 	if (index >= slides.length) {
 		currentSlide = 0
 	} else if (index < 0) {
